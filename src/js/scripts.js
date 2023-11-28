0940bf13c3718bf55ab48893b7fff45d91220593
jvs4nt <joaovit342@gmail.com>
2023-11-28 02:34:22 -0300

    ajuste

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,3 +2,12 @@ function toggleNavbar() {
     var navbarMobile = document.getElementById("navbarMobile");
     navbarMobile.style.display = (navbarMobile.style.display === "block") ? "none" : "block";
 }
+
+window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
