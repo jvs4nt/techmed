bec97225bc808f584b8da938d251f70c75a2fa83
joaosantos <joaovitor.d370@gmail.com>
2023-11-28 01:33:46 -0300

    fst commit

---
+++ src/js/scripts.js
@@ -0,0 +1,4 @@
+function toggleNavbar() {
+    var navbarMobile = document.getElementById("navbarMobile");
+    navbarMobile.style.display = (navbarMobile.style.display === "block") ? "none" : "block";
+}
